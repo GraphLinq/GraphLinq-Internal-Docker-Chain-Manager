0bdbb0506bc78932b6da025f8aaaa2a8e311604d
jguyet <jguyet.weoohh@gmail.com>
2025-11-19 23:41:24 +0100

    Update version to v0.0.33 in publish.sh and release.sh for Docker image tagging and building.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-version=v0.0.14
+version=v0.0.33
 docker tag docker-glq-nodemanager:$version graphlinqchain/docker-glq-nodemanager:$version
 docker push graphlinqchain/docker-glq-nodemanager:$version
 docker tag docker-glq-nodemanager:$version graphlinqchain/docker-glq-nodemanager:latest
--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
 # run it on the destination machine
-docker build --label version=v0.0.14 -t docker-glq-nodemanager:v0.0.14 .
+docker build --label version=v0.0.33 -t docker-glq-nodemanager:v0.0.33 .
